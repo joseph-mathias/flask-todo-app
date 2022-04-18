f533fc845cc531406a7e114e980b5ec6cc3865de
joseph-mathias <nchimunya882@gmail.com>
2022-04-18 13:37:24 +0200

    init app

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
 
 
 class Todo(db.Model):
